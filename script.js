00eff955bebc2d0e8fa77e5ba757237f12d84854
M00R3D <jobmurdan@hotmail.com>
2025-03-23 21:13:32 -0700

    separamos el script

---
+++ script.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(500, 500,WEBGL); 
+    background(blue);        
+    ellipse(0, 0, 80, 40); 
+    describe('A white ellipse on a gray canvas.');
+
+    ellipse(0, 120, 80, 40, 6);
+    describe('A white hexagon on a gray canvas.');
+}
