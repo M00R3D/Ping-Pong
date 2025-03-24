16e16b880e28ae07ee9949bb5da6f0fb2e1f6c04
M00R3D <jobmurdan@hotmail.com>
2025-03-23 21:24:27 -0700

    cree objetos y los dibuje en su posicion inicial

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 function setup() {
     createCanvas(500, 500,WEBGL); 
     background(blue);        
-    ellipse(0, 0, 80, 40); 
-    describe('A white ellipse on a gray canvas.');
 
-    ellipse(0, 120, 80, 40, 6);
-    describe('A white hexagon on a gray canvas.');
+    
+    const bar_a ={
+        x:-220,
+        y:-60,
+        w:20,
+        h:150,
+    }
+    rect(bar_a.x, bar_a.y,bar_a.w, bar_a.h, 6);
+    describe('bar_a');
+    
+    const bar_b ={
+        x:200,
+        y:60,
+        w:20,
+        h:150,
+    }
+    rect(bar_b.x, bar_b.y,bar_b.w, bar_b.h, 6);
+    describe('bar_b');
+
+    const bolita={
+        x:60,
+        y:60,
+        w:6,
+        h:6,
+    }
+    ellipse(bolita.x, bolita.y,bolita.w, bolita.h, 6);
+    describe('bolita');
 }
